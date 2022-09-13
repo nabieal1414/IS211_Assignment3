38187cd0cb72fda6ecab846017382a6bb5f7b0b8
nabieal1414 <112433329+nabieal1414@users.noreply.github.com>
2022-09-13 14:52:23 -0400

    Add files via upload

--- assignment3.py
+++ assignment3.py
@@ -1,9 +1,20 @@
 import argparse
 # other imports go here
-
+import urllib3
+import csv
 
 def main(url):
     print(f"Running main with URL = {url}...")
+    csv_file = 'weblogs.csv'
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    with open(csv_file, 'wb')as file:
+        file.write(r.data)
+    
+    with open(csv_file, 'rt') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print("Some user requested the file {} on {} using a {}. The status of the request was {}, and the file was {} bytes.\n".format(row[0], row[1], row[2], row[3], row[4]))
 
 
 if __name__ == "__main__":
