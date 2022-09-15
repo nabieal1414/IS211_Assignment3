5231f0b517f42f91b613b6170fbc6e5641af7fa5
nabieal1414 <112433329+nabieal1414@users.noreply.github.com>
2022-09-14 23:44:09 -0400

    Add files via upload

--- assignment3.py
+++ assignment3.py
@@ -2,6 +2,7 @@ import argparse
 # other imports go here
 import urllib3
 import csv
+import re
 
 def main(url):
     print(f"Running main with URL = {url}...")
@@ -11,10 +12,51 @@ def main(url):
     with open(csv_file, 'wb')as file:
         file.write(r.data)
     
+    # image hits
+    img_list = ['jpg', 'png', 'gif']
+    no_of_images = 0
     with open(csv_file, 'rt') as f:
         reader = csv.reader(f)
         for row in reader:
-            print("Some user requested the file {} on {} using a {}. The status of the request was {}, and the file was {} bytes.\n".format(row[0], row[1], row[2], row[3], row[4]))
+            if any(re.findall('|'.join(img_list), row[0], re.IGNORECASE)):
+                no_of_images += 1
+            
+    
+    # popular browsers
+    ie = 0
+    firefox = 0
+    chrome = 0
+    safari = 0
+    with open(csv_file, 'rt') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if re.search('MSIE', row[2]):
+                ie += 1
+            elif re.search('Firefox', row[2]):
+                firefox += 1
+            elif re.search('Chrome', row[2]):
+                chrome += 1
+            elif re.search('Safari', row[2]):
+                safari += 1
+
+
+
+    fileObject = csv.reader(open('weblogs.csv'))
+    row_count = sum(1 for row in fileObject)
+    percentage = 100 * float(no_of_images)/float(row_count)
+    print("Image requests account for {}% of all requests".format(percentage))
+
+
+    browsers = {
+        'Safari': safari,
+        'Firefox': firefox,
+        'Chrome': chrome,
+        'IE': ie
+    }
+    popular = max(browsers, key=browsers.get)
+    print("The most popular browser was {}".format(popular))
+
+    
 
 
 if __name__ == "__main__":
